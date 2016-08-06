791449ca46d7ab6d8293d0d2f93a07493a20e9c1
dherg <hergendavid@gmail.com>
2016-08-05 23:02:49 -0400

    Get user id with access token

--- playlistflow.py
+++ playlistflow.py
@@ -5,13 +5,15 @@ import json
 import random
 import string
 import urllib.parse as urlparse
+from playlist import Playlist
+from track import Track
 
 
 def authenticateuser():
-	""" send authorization request, get authorization code in uri, use
+	""" Send authorization request, get authorization code in uri, use
 		code from uri to request access and refresh tokens
 		
-		returns a code or None if some error prevented autherntication
+		Returns a code or None if some error prevented autherntication
 	"""
 	# read application keys from text file
 	with open("keys.txt", "r") as f:
@@ -60,7 +62,8 @@ def requesttokens(code):
 	""" Exchange authorization code for access and refresh tokens with a POST
 		request to /api/token endpoint
 
-		returns access token and refresh token or None,None
+		Returns access token and refresh token, or None,None if unable to 
+		obtain tokens
 	"""
 
 	with open("keys.txt", "r") as f:
@@ -84,10 +87,44 @@ def requesttokens(code):
 	refreshtoken = postresponse["refresh_token"]
 	accesstoken = postresponse["access_token"]
 	expiration = postresponse["expires_in"]
-	print('\ntokens:\n\n{}'.format(r.json()))
-
+	# print('\ntokens:\n\n{}'.format(r.json()))
 
 	return(accesstoken, refreshtoken)
+
+def getuserid(accesstoken):
+	""" Get the current user's id using the /v1/me endpoint
+
+		Returns user's id as a string, or None if unable to obtain id
+	"""
+
+	headers = {}
+	headers["Authorization"] = "Bearer {}".format(accesstoken)
+
+	r = requests.get("https://api.spotify.com/v1/me", headers=headers)
+	# print('response = \n\n {} \n\n'.format(r.text))
+	# print('userid:\n\n{}'.format(r.json()))
+
+
+	getresponse = r.json()
+	if "id" not in getresponse:
+		print('error: getuserid request failed')
+		return(None)
+
+	userid = getresponse["id"]
+	print("userid = {}".format(userid))
+	return(userid)
+
+def getplaylists(accesstoken, userid):
+	""" Build a dict containing the names, thumbnail URLs, and playlist IDs of
+		the current user's playlists using the /v1/me/playlists endpoint
+
+		API returns a maximum of 50 playlists at a time, so while 50 are
+		returned, requests next 50 using the offset API parameter to ensure
+		all playlists are found
+
+		Return a dict with names mapping to playlist ID, 
+	"""
+	pass
 
 def main():
 
@@ -97,7 +134,11 @@ def main():
 	# use code to request access and refresh tokens
 	accesstoken, refreshtoken = requesttokens(code)
 
-	# use these codes to 
+	# get current user id
+	userid = getuserid(accesstoken)
+
+	# build dict (with name, image, and of id's of user's playlists)
+	playlists = getplaylists(accesstoken, userid)
 
 if __name__ == "__main__":
 	main()

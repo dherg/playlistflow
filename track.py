d357a282f05b1f4c8c1166c0aadf148f2111ffdd
dherg <hergendavid@gmail.com>
2016-08-08 16:47:50 -0400

    added print attributes method for track class

--- track.py
+++ track.py
@@ -27,3 +27,42 @@ class Track():
     def __str__(self):
 
         return("trackid:{}".format(self.trackid))
+
+    def printattributes(self):
+
+        print("trackid: {trackid}\n\t"
+            "trackname: {trackname}\n\t" 
+            "artistname: {artistname}\n\t"
+            "albumname: {albumname}\n\t"
+            "danceability: {danceability}\n\t"
+            "energy: {energy}\n\t"
+            "key: {key}\n\t" 
+            "loudness: {loudness}\n\t"
+            "mode: {mode}\n\t"
+            "speechiness: {speechiness}\n\t"
+            "acousticness: {acousticness}\n\t"
+            "instrumentalness: {instrumentalness}\n\t"
+            "liveness: {liveness}\n\t"
+            "valence: {valence}\n\t"
+            "tempo: {tempo}\n\t"
+            "duration_ms: {duration_ms}\n\t"
+            "time_signature: {time_signature}"
+            .format(trackid=self.trackid,
+                trackname=self.trackname,
+                artistname=self.artistname,
+                albumname=self.albumname,
+                danceability=self.danceability,
+                energy=self.energy,
+                key=self.key,
+                loudness=self.loudness,
+                mode=self.mode,
+                speechiness=self.speechiness,
+                acousticness=self.acousticness,
+                instrumentalness=self.instrumentalness,
+                liveness=self.liveness,
+                valence=self.valence,
+                tempo=self.tempo,
+                duration_ms=self.duration_ms,
+                time_signature=self.time_signature
+            )
+        )
